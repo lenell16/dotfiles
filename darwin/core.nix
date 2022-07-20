fa7eb563f49d56eb61dcca93649e20515be46105
lenell16 <lenell16@gmail.com>
2022-07-20 02:13:04 -0700

    Kitty term still weird. clear doesn't work

--- darwin/core.nix
+++ darwin/core.nix
@@ -39,8 +39,8 @@
   # https://github.com/nix-community/home-manager/issues/423
   environment.variables = {
     TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
-    TERMINAL = "kitty";
-    TERM = "kitty";
+    # TERMINAL = "kitty";
+    # TERM = "kitty";
   };
 
   # Auto upgrade nix package and the daemon service.
