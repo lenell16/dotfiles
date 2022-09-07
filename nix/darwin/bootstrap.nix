b1729a7eb8261e80785f8d7420c923cd7eacd8ae
lenell16 <lenell16@gmail.com>
2022-09-07 12:20:55 -0700

    working

--- nix/darwin/bootstrap.nix
+++ nix/darwin/bootstrap.nix
@@ -21,7 +21,7 @@
   users.nix.configureBuildUsers = true;
 
   # Enable experimental nix command and flakes
-  # nix.package = pkgs.nixUnstable;
+  nix.package = pkgs.nixUnstable;
   nix.extraOptions = ''
     auto-optimise-store = true
     experimental-features = nix-command flakes
