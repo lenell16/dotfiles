6b5adfc8b47b2ca9354602dd672258a7749b6db4
Alonzo Thomas <lenell16@gmail.com>
2023-06-25 15:58:54 -0700

    Update lazynvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"nvim-treesitter",
-		dependencies = {
-			{
-				"axelvc/template-string.nvim",
-				event = { "InsertEnter" },
-				opts = {
-					filetypes = { "typescript", "javascript", "typescript", "javascriptreact" },
-					jsx_brackets = true,
-				},
-				config = function(_, opts)
-					require("template-string").setup(opts)
-				end,
-			},
-			{ "RRethy/nvim-treesitter-endwise", event = { "InsertEnter" } },
-		},
-		opts = {
-			endwise = {
-				enable = true,
-			},
-		},
-	},
+  {
+    "nvim-treesitter",
+    dependencies = {
+      {
+        "axelvc/template-string.nvim",
+        event = { "InsertEnter" },
+        opts = {
+          filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+          jsx_brackets = true,
+        },
+        config = function(_, opts)
+          require("template-string").setup(opts)
+        end,
+      },
+      { "RRethy/nvim-treesitter-endwise", event = { "InsertEnter" } },
+    },
+    opts = {
+      endwise = {
+        enable = true,
+      },
+    },
+  },
 }
