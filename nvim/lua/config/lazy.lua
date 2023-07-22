f3a9faa964d8cc3550c87b10df8abe2d20fd9a96
Alonzo Thomas <lenell16@gmail.com>
2023-07-21 23:34:51 -0700

    Remove unused plugin and comment out another plugin import
    - Remove the "lazyvim.plugins.extras.editor.flash" plugin import
    - Comment out the "lazyvim.plugins.extras.ui.mini-animate" plugin import

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,8 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
