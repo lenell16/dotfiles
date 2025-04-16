ecc5114973dac66e2c0b9c0601fa4c7e197d2f4c
lenell16 <lenell16@gmail.com>
2025-04-16 14:05:27 -0700

    feat: add Adobe Acrobat Reader to Nix packages

--- home/packages.nix
+++ home/packages.nix
@@ -24,6 +24,7 @@
     curl             # Data transfer tool
     fd               # Alternative to find
     ripgrep          # Fast grep replacement
+    adobe-acrobat-reader # PDF reader
     podman           # Container management tool
     docker-client    # Docker CLI client
     
